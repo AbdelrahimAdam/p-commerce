64b2485588a863cab07e00660cb3f03c5e7bbb1b
AbdelrahimAdam <hemaadam2121@gmail.com>
2026-03-25 17:47:52 +0200

    fix: add named export to AppFooter component

--- src/components/Layout/index.ts
+++ src/components/Layout/index.ts
@@ -1,5 +1,4 @@
 // src/components/Layout/index.ts
-export { default as LuxuryHeader } from './LuxuryHeader.vue'
 export { default as AppFooter } from './AppFooter.vue'
 export { default as MarketingLayout } from './MarketingLayout.vue'
-export { default as LandingLayout } from './LandingLayout.vue' // if it exists
+
