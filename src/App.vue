415893d9534b04dcc3fb3a0b273cc8302598f639
AbdelrahimAdam <hemaadam2121@gmail.com>
2026-03-25 17:43:10 +0200

    update project

--- src/App.vue
+++ src/App.vue
@@ -102,10 +102,7 @@
                     :title="isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'"
                   >
                     <svg v-if="isDarkMode" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                      <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 
-001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 
-0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 
-01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"/>
+                      <path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"/>
                     </svg>
                     <svg v-else class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                       <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"/>
@@ -121,13 +118,11 @@
                     >
                       <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
-                              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 
-11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+                              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
                       </svg>
                       <span 
                         v-if="unreadNotifications > 0"
-                        class="absolute -top-1 -right-1 w-4 h-4 sm:w-5 sm:h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center 
-text-[10px] sm:text-xs"
+                        class="absolute -top-1 -right-1 w-4 h-4 sm:w-5 sm:h-5 bg-red-500 text-white text-xs rounded-full flex items-center justify-center text-[10px] sm:text-xs"
                       >
                         {{ unreadNotifications > 9 ? '9+' : unreadNotifications }}
                       </span>
@@ -136,8 +131,7 @@ text-[10px] sm:text-xs"
                     <!-- Notifications Dropdown - Mobile Optimized -->
                     <div 
                       v-if="showNotifications"
-                      class="fixed sm:absolute right-0 sm:right-auto left-0 sm:left-auto mt-0 sm:mt-2 w-full sm:w-80 max-w-[calc(100vw-2rem)] sm:max-w-none 
-bg-white rounded-lg shadow-luxury-xl border border-gray-200 z-50 top-16 mx-4 sm:mx-0 max-h-[calc(100vh-8rem)] sm:max-h-96 overflow-hidden flex flex-col"
+                      class="fixed sm:absolute right-0 sm:right-auto left-0 sm:left-auto mt-0 sm:mt-2 w-full sm:w-80 max-w-[calc(100vw-2rem)] sm:max-w-none bg-white rounded-lg shadow-luxury-xl border border-gray-200 z-50 top-16 mx-4 sm:mx-0 max-h-[calc(100vh-8rem)] sm:max-h-96 overflow-hidden flex flex-col"
                       v-click-outside="closeNotifications"
                     >
                       <div class="p-4 border-b border-gray-200 flex-shrink-0">
@@ -149,8 +143,7 @@ bg-white rounded-lg shadow-luxury-xl border border-gray-200 z-50 top-16 mx-4 sm:
                         <div v-if="notifications.length === 0" class="p-8 text-center text-gray-500">
                           <svg class="w-12 h-12 mx-auto mb-2 text-gray-300" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" 
-                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 
-11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
+                                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"/>
                           </svg>
                           <p>{{ safeTranslate('No notifications') }}</p>
                         </div>
@@ -421,7 +414,7 @@ import { getLocalizedTitle } from '@/router/routes'
 // Luxury Components
 import SEOHead from '@/components/UI/SEOHead.vue'
 import LuxuryHeader from '@/components/Layout/LuxuryHeader.vue'
-import AppFooter from '@/components/Layout/AppFooter.vue'
+import { AppFooter } from '@/components/Layout'
 import LuxuryCartSidebar from '@/components/Cart/LuxuryCartSidebar.vue'
 import LuxurySearchModal from '@/components/search/SearchModal.vue'
 import LuxuryNotificationCenter from '@/components/UI/LuxuryNotificationCenter.vue'
---
+++ src/components/Layout/index.ts
@@ -0,0 +1,5 @@
+// src/components/Layout/index.ts
+export { default as LuxuryHeader } from './LuxuryHeader.vue'
+export { default as AppFooter } from './AppFooter.vue'
+export { default as MarketingLayout } from './MarketingLayout.vue'
+export { default as LandingLayout } from './LandingLayout.vue' // if it exists
